c53688fd3aa80f3a642e88f33c07e0cc99ba5ac4
niloheri123-ux <niloheri123@gmail.com>
2026-06-18 17:26:14 -0400

    arreglo de una pull request

--- src/main/java/RedNorte/Gestion/model/CitaMedica.java
+++ src/main/java/RedNorte/Gestion/model/CitaMedica.java
@@ -1,5 +1,6 @@
 package RedNorte.Gestion.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
